a57ed4e25bde264e78856457cfd27bf0a13bdc95
Menashe <mborukhov770@gmail.com>
2023-03-08 16:29:29 -0700

    operations p2

--- calc/app.py
+++ calc/app.py
@@ -38,3 +38,23 @@ def do_div():
     result = add(a, b)
 
     return div(result)
+
+
+# BONUS
+operators = {
+    "add": add,
+    "sub": sub,
+    "mult": mult,
+    "div": div,
+}
+
+
+@app.route("/math/<oper>")
+def do_math(oper):
+    """Do math on a and b."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = operators[oper](a, b)
+
+    return str(result)
